81bdb5c3fca3c48d561056ca019c0155797da5db
Matheus Lima <matheus.lima@lavid.ufpb.br>
2018-08-18 12:46:20 -0300

    Modifica Robo resultado

--- src/frontend/CadeMeuProjetoBot/src/views/ResultsScreen/styles.js
+++ src/frontend/CadeMeuProjetoBot/src/views/ResultsScreen/styles.js
@@ -4,12 +4,13 @@ import { metrics, colors } from '../../styles';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     padding: metrics.distance,
     backgroundColor: colors.primary,
   },
   robotImage: {
     resizeMode: 'contain',
-    height: metrics.robotHeight / 1.8,
+    height: metrics.robotHeight / 1.5,
   },
 });
 
