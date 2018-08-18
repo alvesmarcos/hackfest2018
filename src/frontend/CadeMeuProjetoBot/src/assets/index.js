2e1c56655aee4a8225235d623a5bf2cddad88959
Matheus Lima <matheus.lima@lavid.ufpb.br>
2018-08-18 11:56:02 -0300

    Adiciona assets

---
+++ src/frontend/CadeMeuProjetoBot/src/assets/index.js
@@ -0,0 +1,25 @@
+const apenasRosto = require('./apenas_rosto.png');
+const roboApaixonado = require('./robo_apaixonado.png');
+const roboConfuso = require('./robo_confuso.png');
+const roboDuvida = require('./robo_duvida.png');
+const roboFeliz = require('./robo_feliz.png');
+const roboHome = require('./robo_home.png');
+const roboInputVazio = require('./robo_input_vazio.png');
+const roboResultadoFeliz = require('./robo_resultado_feliz.png');
+const roboResultadoTriste = require('./robo_resultado_triste.png');
+const rostoComFundo = require('./rosto_com_fundo.png');
+const roboUsuarioDigitando = require('./usuario_digitando.png');
+
+export {
+  apenasRosto,
+  roboApaixonado,
+  roboConfuso,
+  roboDuvida,
+  roboFeliz,
+  roboHome,
+  roboInputVazio,
+  roboResultadoFeliz,
+  roboResultadoTriste,
+  rostoComFundo,
+  roboUsuarioDigitando,
+};
