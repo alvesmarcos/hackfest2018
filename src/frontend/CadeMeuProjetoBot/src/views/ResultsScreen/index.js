3735e87027528f0cc0579ab3f38759f0263be0f2
alvesmarcos <alves.henrique.marcos@gmail.com>
2018-08-19 01:00:03 -0300

    Fix redux/actions

--- src/frontend/CadeMeuProjetoBot/src/views/ResultsScreen/index.js
+++ src/frontend/CadeMeuProjetoBot/src/views/ResultsScreen/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { View, Image } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
+import { connect } from 'react-redux';
 
 import { roboResultadoFeliz } from '../../assets/index';
 import styles from './styles';
@@ -9,97 +10,7 @@ import { Input } from '../../components';
 import RenderCard from './components/RenderCard';
 import { metrics } from '../../styles';
 
-const exampleData = [
-  {
-    numero: '805/2018',
-    ementa:
-      'OBRIGA AS PESSOAS JURÍDICAS QUE OPERAM PLANOS DE ASSISTÊNCIA ODONTOLÓGICA NO MUNICÍPIO DE JOÃO PESSOA A SE ADEQUAREM AOS VALORES REFERENCIAIS DA CLASSIFICAÇÃO BRASILEIRA HIERARQUIZADA DE PROCEDIMENTOS ODONTOLÓGICOS.',
-    indexacao: 'PREÇO TRATAMENTO DENTÁRIO DENTE ODONTÓLOGO ODONTOLOGIA',
-    autor: 'Raíssa Gomes Lacerda Rodrigues de Aquino',
-    apelido: 'PROCEDIMENTOS ODONTOLÓGICOS',
-    objeto:
-      'OBRIGA AS PESSOAS JURÍDICAS QUE OPERAM PLANOS DE ASSISTÊNCIA ODONTOLÓGICA NO MUNICÍPIO DE JOÃO PESSOA A SE ADEQUAREM AOS VALORES REFERENCIAIS DA CLASS',
-    emTramitacao: true,
-    materiaPolemica: false,
-    regimeTramitacao: 'Normal',
-    textoIntegral: 'http://177.200.32.195:9673/sapl/sapl_documentos/materia/78450_texto_integral',
-    tramites: [
-      {
-        data: '15/08/2018',
-        origem: 'PARLAMENTAR',
-        destino: 'Secretaria Legislativa',
-        turno: 'Único',
-        situacao: 'ENVIADO PARA LEITURA EM PLENÁRIO',
-        ultimaAcao: 'ENVIADO PARA A LEITURA EM PLENÁRIO NO DIA 21/08/2018.',
-      },
-    ],
-  },
-  {
-    numero: '482/2006',
-    ementa: 'DENOMINA DE RUA INÁCIA PESSOA DA SILVA',
-    indexacao: 'RUA INÁCIA PESSOA DA SILVA',
-    autor: 'Luciano Cartaxo Pires',
-    apelido: 'LOGRADOURO PÚBLICO',
-    objeto: 'NOME DE RUA',
-    emTramitacao: false,
-    materiaPolemica: false,
-    regimeTramitacao: 'Normal',
-    textoIntegral: 'http://177.200.32.195:9673/sapl/sapl_documentos/materia/1055_texto_integral',
-    tramites: [
-      {
-        data: '10/05/2006',
-        origem: 'Executivo PMJP',
-        destino: 'Secretaria Legislativa',
-        turno: 'Único',
-        situacao: 'L E I',
-        ultimaAcao:
-          'LEI Nº 10.758, DE 10/05/2006 PUBLICADA NO SEMANÁRIO MUNICIPAL Nº 1009, DE 14 A 20/05/2006',
-      },
-      {
-        data: '19/04/2006',
-        origem: 'Plenário',
-        destino: 'Executivo PMJP',
-        turno: 'Único',
-        situacao: 'Aprovado em Plenário',
-        ultimaAcao: 'AUTÓGRAFO Nº 040/2006 SANÇÃO OFICIO Nº 011/DDA/2006 DE 25/04/2006',
-      },
-      {
-        data: '22/03/2006',
-        origem: 'Plenário',
-        destino: 'Comissão de Constituição, Justiça, Redação e Legis. Part.',
-        turno: 'Único',
-        situacao: 'Aguardando Parecer',
-        ultimaAcao: 'ENCAMINHANDO PARA COMISSÃO DE CONSTITUIÇÃO E JUSTIÇA EM 23/03/2006',
-      },
-    ],
-  },
-  {
-    numero: '805/2018',
-    ementa:
-      'OBRIGA AS PESSOAS JURÍDICAS QUE OPERAM PLANOS DE ASSISTÊNCIA ODONTOLÓGICA NO MUNICÍPIO DE JOÃO PESSOA A SE ADEQUAREM AOS VALORES REFERENCIAIS DA CLASSIFICAÇÃO BRASILEIRA HIERARQUIZADA DE PROCEDIMENTOS ODONTOLÓGICOS.',
-    indexacao: 'PREÇO TRATAMENTO DENTÁRIO DENTE ODONTÓLOGO ODONTOLOGIA',
-    autor: 'Raíssa Gomes Lacerda Rodrigues de Aquino',
-    apelido: 'PROCEDIMENTOS ODONTOLÓGICOS',
-    objeto:
-      'OBRIGA AS PESSOAS JURÍDICAS QUE OPERAM PLANOS DE ASSISTÊNCIA ODONTOLÓGICA NO MUNICÍPIO DE JOÃO PESSOA A SE ADEQUAREM AOS VALORES REFERENCIAIS DA CLASS',
-    emTramitacao: true,
-    materiaPolemica: false,
-    regimeTramitacao: 'Normal',
-    textoIntegral: 'http://177.200.32.195:9673/sapl/sapl_documentos/materia/78450_texto_integral',
-    tramites: [
-      {
-        data: '15/08/2018',
-        origem: 'PARLAMENTAR',
-        destino: 'Secretaria Legislativa',
-        turno: 'Único',
-        situacao: 'ENVIADO PARA LEITURA EM PLENÁRIO',
-        ultimaAcao: 'ENVIADO PARA A LEITURA EM PLENÁRIO NO DIA 21/08/2018.',
-      },
-    ],
-  },
-];
-
-export default class ResultsScreen extends Component {
+class ResultsScreen extends Component {
   static propTypes = {
     navigation: PropTypes.shape({
       getParam: PropTypes.func,
@@ -129,11 +40,13 @@ export default class ResultsScreen extends Component {
   );
 
   render() {
+    const { data } = this.props;
+    //--
     return (
       <View style={styles.container}>
         {this.renderHeader()}
         <Carousel
-          data={exampleData}
+          data={data}
           renderItem={this.renderItem}
           sliderWidth={metrics.sliderWidth}
           itemWidth={metrics.itemWidth}
@@ -142,3 +55,9 @@ export default class ResultsScreen extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  data: state.dataReducer,
+});
+
+export default connect(mapStateToProps, {})(ResultsScreen);
